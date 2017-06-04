7a8684c4d5f368ff6871b4a6811195ffc8c20caa
Mark Luetke <luetkemj@gmail.com>
2017-06-04 10:44:42 -0500

    :sparkles: init

---
+++ README.md
@@ -0,0 +1,11 @@
+[![Commitizen friendly](https://img.shields.io/badge/commitizen-friendly-brightgreen.svg)](http://commitizen.github.io/cz-cli/)
+
+# scribe hex map
+
+## Getting started
+
+## Features
+
+## Contributing
+
+Feature requests, issues, and contributions are all welcome.
---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const webpack = require('webpack');
+
+module.exports = {
+  plugins: [
+    new webpack.optimize.UglifyJsPlugin({
+      minimize: true,
+      sourceMap: true,
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|build|coverage|dist|test)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['env'],
+          },
+        },
+      },
+    ],
+  },
+  entry: './src/index.js',
+  output: {
+    filename: 'dist/scribe-hexmap.min.js',
+  },
+};
