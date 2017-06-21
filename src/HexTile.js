15711dce55f113c2a62d33a5a89f471940143779
Mark Luetke <luetkemj@gmail.com>
2017-06-21 08:51:22 -0500

    HexTile class
    replace old HexTile class with Tile class that expects the schema from
    my own hex math

--- src/HexTile.js
+++ src/HexTile.js
@@ -1,15 +1,13 @@
-export default class HexTile extends PIXI.Sprite {
+export default class Tile extends PIXI.Sprite {
   constructor(texture, hex, config) {
     super(texture);
 
     this.hex = hex;
 
-    const point = hex.toPoint();
-
-    this.x = point.x;
-    this.y = point.y;
-    this.width = hex.width();
-    this.height = hex.height();
+    this.x = hex.point && hex.point.x;
+    this.y = hex.point && hex.point.y;
+    this.width = hex.width;
+    this.height = hex.height;
 
     if (this.hex.isSeed) {
       if (config.showSeeds) {
