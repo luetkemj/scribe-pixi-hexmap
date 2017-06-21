d9caaf3c27566a76884e8c00c1c4e102c026c566
Mark Luetke <luetkemj@gmail.com>
2017-06-21 08:53:13 -0500

    use new HexTile class
    refactor to use new HexTile class

--- src/index.js
+++ src/index.js
@@ -2,8 +2,7 @@ import { each } from 'lodash';
 import world, { world2 } from './world/test';
 // import world, { world2 } from './world/world';
 
-// import HexTile from './HexTile';
-import Tile from './Tile';
+import HexTile from './HexTile';
 
 const app = new PIXI.Application({ antialias: true, width: 975 });
 
@@ -11,14 +10,9 @@ const mapDom = document.getElementById('map');
 mapDom.appendChild(app.view);
 
 each(world, (hex) => {
-  const tile = new Tile(hex.texture, hex, { showSeeds: false });
+  const tile = new HexTile(hex.texture, hex, { showSeeds: false });
   app.stage.addChild(tile);
 });
-// world.forEach((hex) => {
-//   const tile = new Tile(hex.texture, hex, { showSeeds: false });
-//   app.stage.addChild(tile);
-// });
-
 
 // map 2
 const app2 = new PIXI.Application({ antialias: true, width: 1950, height: 1150 });
@@ -27,10 +21,6 @@ const mapDom2 = document.getElementById('map2');
 mapDom2.appendChild(app2.view);
 
 each(world2, (hex) => {
-  const tile = new Tile(hex.texture, hex, { showSeeds: true });
+  const tile = new HexTile(hex.texture, hex, { showSeeds: true });
   app2.stage.addChild(tile);
 });
-// world2.forEach((hex) => {
-//   const tile = new Tile(hex.texture, hex, { showSeeds: true });
-//   app2.stage.addChild(tile);
-// });
